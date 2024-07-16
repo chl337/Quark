f47e012298d3ffd6fde8d2537d8f02b9e5201e34
Yiliang Dong <dongyiliangsteven@163.com>
2024-07-16 15:22:03 +0200

    tdx: add idt support
    Previously #VE is used for custom interrupt injection. Now tdx uses the
    interrupt for interception, thus move the previous #VE handler to
    interrupt vector 22, and add new #VE handler.

--- qkernel/src/lib.rs
+++ qkernel/src/lib.rs
@@ -46,6 +46,7 @@ extern crate scopeguard;
 extern crate serde_derive;
 extern crate log;
 extern crate spin;
+extern crate tdx_tdcall;
 #[cfg(target_arch = "x86_64")]
 extern crate x86_64;
 extern crate xmas_elf;

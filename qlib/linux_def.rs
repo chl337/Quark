d5c2724b219d6c2151f22a56e2a7ed9e4fe28672
Yiliang Dong <dongyiliangsteven@163.com>
2024-12-18 15:55:48 +0000

    sev-snp: map backend memory for special pages

--- qlib/linux_def.rs
+++ qlib/linux_def.rs
@@ -3111,6 +3111,14 @@ impl MemoryDef {
     pub const SHIM_MEMORY_BASE: u64 = 0x0;
     pub const SHIM_MEMORY_SIZE: u64 = 2 * Self::ONE_GB;
 }
+
+#[cfg(feature = "snp")]
+impl MemoryDef {
+    pub const CPUID_PAGE: u64 = Self::PHY_LOWER_ADDR + Self::QKERNEL_IMAGE_SIZE;
+    pub const SECRET_PAGE: u64 = Self::CPUID_PAGE + Self::PAGE_SIZE;
+    pub const GHCB_OFFSET: u64 = Self::SECRET_PAGE + Self::PAGE_SIZE;
+}
+
 //mmap prot
 pub struct MmapProt {}
 

de7551d0aea6af57befab48cdefb06e77e7e3585
ch3lt <christob.tr@hotmail.com>
2025-03-11 15:26:41 +0100

    cc: attestation: add defintion for KeyBrockerService client

--- qkernel/src/lib.rs
+++ qkernel/src/lib.rs
@@ -64,6 +64,7 @@ extern crate serde;
 extern crate jwt_compact;
 extern crate kbs_types;
 extern crate getrandom;
+extern crate zeroize;
 
 use core::panic::PanicInfo;
 use core::sync::atomic::{AtomicI32, AtomicUsize, Ordering};

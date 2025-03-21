d71c00f799acca56510f0a1f30d4865429c2fe63
ch3lt <christob.tr@hotmail.com>
2025-03-21 12:51:11 +0100

    cc: attestation: x86_64: tdx: include as attestable platform
    (cherry picked from commit 9f9ed6ece43162d7c2d914688d37f53354c9d4cf)

--- qlib/config.rs
+++ qlib/config.rs
@@ -183,6 +183,7 @@ impl TryFrom<CCMode> for String {
             CCMode::Normal => String::from("normal"),
             CCMode::NormalEmu => String::from("normalemu"),
             CCMode::SevSnp => String::from("snp"),
+            CCMode::TDX => String::from("tdx"),
             _ => String::from("none"),
         };
 
